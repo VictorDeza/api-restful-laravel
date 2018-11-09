2d3490677e199015c918885d9d971406687d2354
VictorDza <victor.junior.dza@gmail.com>
2018-11-09 14:11:42 -0500

    Cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Spatie\Cors\Cors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -41,6 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
